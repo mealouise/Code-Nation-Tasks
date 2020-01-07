cb85288991268841343ef22712246bfae3cb5c99
Emma Sheridan <emmasheridan@Emmas-MacBook-Air.local>
2020-01-07 22:56:14 +0000

    improved task 5

--- week1/task5.js
+++ week1/task5.js
@@ -6,17 +6,16 @@
     where someone is at times of the day. E.g. if the time
     is 7 I’m at home, at 8 I’m commuting, at 9 I’m at work.
 */
-let time = 20
+let time = 2000;
+let placeOfWork = 'learning at code nation';
+let townOfHome = 'home in urmston';
 
-if ( time >= 0 && time < 6) {
-    console.log(`At ${time} I'm at home.`)
+if (time >= 600 && time < 800) {
+    console.log(`At ${time} I'm commuting.`);
 }
-    else if (time >= 6 && time < 8) {
-        console.log(`At ${time} I'm commuting.`)
-    }
-    else if (time >= 9 && time < 17) {
-        console.log(`At ${time} I'm at work.`)
-    }
-    else {
-        console.log("I'm in bed")
-    }
+else if (time >= 800 && time < 1700) {
+    console.log(`At ${time} I'm at ${placeOfWork}.`);
+}
+else {
+    console.log(`At ${time} I'm at ${townOfHome}.`);
+}
