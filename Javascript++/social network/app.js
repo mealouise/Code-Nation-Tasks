53079a140c3ad294cefeed546d21a02e17f5b580
Emma Sheridan <emmasheridan@Emmas-MacBook-Air.local>
2020-01-28 22:06:58 +0000

    email&password validation added along with signin/out functions

--- Javascript++/social network/app.js
+++ Javascript++/social network/app.js
@@ -8,40 +8,66 @@ const emailDetails = document.getElementById("email");
 const passwordDetails = document.getElementById("password");
 
 
+//can do initally set in css
+// signOutButton.style.display ="none";
 
-signOutButton.style.visibility ="hidden";
-
-let email;
-let password;
-let signedIn = true;
-
-const signedInFunction = () => {
-    if (signedIn){
-        console.log(`I am true`);
-        
-        if (emailDetails.value == "") {
+// let email;
+// let password;
+let signedIn = false; //not signed in to begin with so has to be false
+const signInFunction = () => {
+    // ! means not equal to
+    if (!signedIn){
+        console.log(`I am true`) 
+        if (!emailDetails.value) {
             return alert("Please enter your email")
         }
-        if (passwordDetails.value == "") {
+        if (!passwordDetails.value) {
             return alert("Please enter your password")
         }
-        signedIn = false;
-        userDetails.style.visibility = "hidden";
+        // if (passwordDetails.value !== "winston" && ) {
+        //     return alert("You have entered the wrong password")
+        // }
+            
+        if (emailDetails.value === "esheridan45@gmail.com" && passwordDetails.value === "winston")  {
+            alert("You are now logged in!") //no return here as want the function to keep running
+            // console.log("is esheridan");
+            
+        } else {
+            return alert("Your credentials are incorrect")
+           
+        }
+
+        signedIn = true; //user has filled in their email and password
+        userDetails.style.display = "none";
         userSignedIn.textContent = "Hello you are signed in!";
-        signOutButton.style.visibility ="visible";
-     
-    } else {
-        console.log(`I am false.`);
-        signedIn = true;
-        // signInButton.style.visibiity = "visible";
+        signInButton.style.display = "none";
+        signOutButton.style.display = "block";
+
     }
 
 }
 
+const signOutFunction = () => {
+    signedIn = false;
+    console.log("signed out button called")
+    userDetails.style.display ="block";
+    signOutButton.style.display = "none";
+    signInButton.style.display = "block";
+    emailDetails.value = "";
+    passwordDetails.value = "";
+
+}
+
+
+
 
 signInButton.addEventListener("click", () => {
-    signedInFunction()
+    signInFunction();
 
+})
+
+signOutButton.addEventListener("click", () => {
+    signOutFunction();
 })
 
 
