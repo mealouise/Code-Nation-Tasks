c8c21cc6dc01f6f62b27abb311740f336c3c888d
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-03 17:13:38 +0000

    click event added & started adding the DOM

--- Javascript++/Top Trumps game/app.js
+++ Javascript++/Top Trumps game/app.js
@@ -1,3 +1,20 @@
+const play = document.getElementById("play");
+const cardDeck1 = document.getElementById("cardDeck1");
+const cardDeck2= document.getElementById("cardDeck2");
+const player1Status = document.getElementById("player1Status");
+const player2Status = document.getElementById("player2Status");
+const animalImageOne = document.getElementById("animalImage");
+const animalImageTwo = document.getElementById("animalImage");
+
+
+
+
+
+
+const nameOne = document.getElementById("nameOne");
+const nameTwo = document.getElementById("nameTwo");
+// const speedButton = document.getElementById("speed");
+
 
 let player1CardNum;
 let player2CardNum;
@@ -24,17 +41,13 @@ const cardDeck = [{
 }];
 
 
-//function that combines everything together
-
-
-
 //function that shows current card
 const showCard = () => {
     // player1Cards[0] = currentCardP1;
     // player2Cards[0] = currentCardP2;
     console.log("Player 1's cards are" + JSON.stringify(player1Cards[0]));
     console.log("Player2's cards are" + JSON.stringify(player2Cards[0]));
-    
+
 }
 
 
@@ -44,46 +57,52 @@ const distributeCards = () => {
         if ( i % 2 == 0) {
             
             player1Cards.push(cardDeck[i]);
-            player1CardNum = player1Cards.length;
+            // cardDeck1.textContent = `Card Deck 1: ${player1Cards.length}`;
         } else {
             
             player2Cards.push(cardDeck[i]);
-            player2CardNum = player2Cards.length;
+            // cardDeck2.textContent = `Card Deck 2: ${player2Cards.length}`;
         }
         // cardDeck = [];
        
+    
 }
+console.log(player2Cards);
+console.log(player1Cards);
+console.log('-----------');
 // score();
 }
 distributeCards();
 
 
-const score = () => {
-    // console.log(player2Cards);
-    
+const takeCard = () => {
+    showCard();
+
+
     if (player1Cards[0].speed > player2Cards[0].speed) {
 
         // player1CardNum++;
         // player2CardNum--;
         let removedCardP1  = player2Cards.shift(); 
-        
         player1Cards.push(removedCardP1);
         
-        
-        //card and position of that card
-    // } else if (player1Cards[0].speed === player2Cards[0].speed){ // creating a holdingpile
-    //     const holdingPile = player1Cards.shift();
+        removedCardP1 = player1Cards.shift()
+        player1Cards.push(removedCardP1)
 
-    } else {
+    } else if (player2Cards[0].speed > player1Cards[0].speed) {
+    
         // player1CardNum--;
         // player2CardNum++;
         let removedCardP2 = player1Cards.shift();
         player2Cards.push(removedCardP2);
+
+        removedCardP2 = player2Cards.shift()
+        player2Cards.push(removedCardP2)
     } 
-
-    console.log(player2Cards);
-
 }
+
+//////////// } else if (player1Cards[0].speed === player2Cards[0].speed){ // creating a holdingpile
+    //     const holdingPile = player1Cards.shift();
 
 const reset = () => {
     player1CardNum = 0;
@@ -92,43 +111,54 @@ const reset = () => {
     player2Cards = [];
     
     }
+
+
+const winOrLose = () => {
+    if (player1Cards.length === 0) {
+        console.log("Player 1 has won!")
+        player1Status.textContent = "Winner!";
+    } else if (player2Cards.length === 0) {
+        console.log("Player 2 has won!")
+        player2Status.textContent = "Winner!";
+    }
+    reset();
+}
+    console.log(player1Cards);
+    console.log(player2Cards);
+    console.log('-----------');
+    
+
+
+play.addEventListener("click", () => {
+    takeCard();
+    winOrLose();
+
+    cardDeck1.textContent = `Card Deck 1: ${player1Cards.length}`;
+    cardDeck2.textContent = `Card Deck 2: ${player2Cards.length}`;
+    
+    nameOne.textContent = player1Cards[0].name;
+    nameTwo.textContent = player2Cards[0].name;
     
     
+
+
+})  
+
+
 
 
 
 // console.log(cardDeck.length)
-// function to distribute the cards between 2 players
-
-/**
- * 
- * while(true)
- * 
- *      p1 || p2 = 0
- *          return winningplayer
- */
-const winningPlayer = () => {
-
-    while (player1Cards.length > 1 || player2Cards.length > 1) {
-        score();
-        if (player1Cards.length === 0 || player2Cards.length === 0)
-        console.log(`The winning player is `)
-        reset(); 
-
-
-
-        
-    // console.log("player has won")
-    for (let i=0; i < cardDeck.length; i++) {
-        if (player1Cards.length === 0 || player2Cards.length === 0) {
-        console.log("Player has won");
-        reset(); 
-        }
-        else {
-        score() 
-    }
-}
-}
+// function to distribute the cards between 2 players   
+    // // console.log("player has won")
+    // for (let i=0; i < cardDeck.length; i++) {
+    //     if (player1Cards.length === 0 || player2Cards.length === 0) {
+    //     console.log("Player has won");
+    //     reset(); 
+    //     }
+    //     else {
+    //     score() 
+    // }
 
 
 
@@ -136,57 +166,26 @@ const winningPlayer = () => {
 // console.log("Player 1's cards are" + JSON.stringify(player1Cards));
 // console.log("Player2's cards are" +JSON.stringify(player2Cards));
 
-showCard();
-score();
+// score();
+// score();
+// score();
+// winningPlayer();
 
 console.log("Player 1's cards are: " + JSON.stringify(player1Cards));
 console.log("Player 2's cards are " + JSON.stringify(player2Cards));
-winningPlayer();
+// winningPlayer();
 
 
+// const winningPlayer = () => { //POINTER - help with the loop to loop throughthe score function and declare a winner
 
-
-
-
-
-
-
-
-
-
-
-// const playGame = () => {
-//     distributeCards();
-//     score();
-//     winningPlayer();
+//     while (player1Cards.length > 0 || player2Cards.length > 0) {
+//         score();
+//         if (player1Cards.length === 0) {
+//             return ("Player 1 has won!")
+//         }
+//         if (player2Cards.length === 0) {
+//         return ("Player 2 has won!")
+//         }
+//         reset(); 
 // }
-
-// playGame();
-
-// console.log(player1CardNum);
-// console.log(player2CardNum);
-// have object in a string so can't print the usual way, have to add commar to seperate
-// console.log(`Player1's cards are: ', ${player1Cards[0]}`);
-// console.log(`Player2's cards are: ', ${player2Cards[0]}`);
-// console.log(cardDeck);
-
-//function if player1 wins
-// const score = () => {
-//     if (player1Cards[0].speed > players2Cards[0].speed) {
-//         player1CardNum++;
-//         player2CardNum--;
-//         let removedCardP1  = player1Cards.splice(0,1); 
-//         player1Cards.push(removedCardP1);
-        
-        
-//         //card and position of that card
-//     // } else if (player1Cards[0].speed === player2Cards[0].speed){ // creating a holdingpile
-//     //     const holdingPile = player1Cards.shift();
-
-//     } else {
-//         player1CardNum--;
-//         player2CardNum++;
-//         let removedCardP2 = player2Cards.splice(0,1);
-//         player2Cards.push(removedCardP2);
-//     } 
 // }
--- Javascript++/Top Trumps game/workingfile.js
+++ Javascript++/Top Trumps game/workingfile.js
@@ -30,3 +30,180 @@
 // ]
 
 // console.log(card1[0].health < card1[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let player1CardNum;
+let player2CardNum;
+
+
+let player1Cards = [];
+let player2Cards = [];
+
+const cardDeck = [{
+    name: "Cheetah",
+    speed: 104
+},
+{
+    name: "Gray Wolf",
+    speed: 48
+},
+{
+    name: "Polar Bear",
+    speed: 24
+},
+{
+    name: "Leopard",
+    speed: 64
+}];
+
+
+const showCard = () => {
+    // player1Cards[0] = currentCardP1;
+    // player2Cards[0] = currentCardP2;
+    console.log("Player 1's cards are" + JSON.stringify(player1Cards[0]));
+    console.log("Player2's cards are" + JSON.stringify(player2Cards[0]));
+    
+}
+
+
+
+const distributeCards = () => {
+    for (let i=0; i < cardDeck.length; i++) {
+        if ( i % 2 == 0) {
+            
+            player1Cards.push(cardDeck[i]);
+            player1CardNum = player1Cards.length;
+        } else {
+            
+            player2Cards.push(cardDeck[i]);
+            player2CardNum = player2Cards.length;
+        }
+        // cardDeck = [];
+       
+}
+// score();
+}
+distributeCards();
+
+
+const score = () => {
+    // console.log(player2Cards);
+    
+    if (player1Cards[0].speed > player2Cards[0].speed) {
+
+        // player1CardNum++;
+        // player2CardNum--;
+        let removedCardP1  = player2Cards.shift(); 
+        player1Cards.push(removedCardP1);
+        
+        
+
+    } else {
+        // player1CardNum--;
+        // player2CardNum++;
+        let removedCardP2 = player1Cards.shift();
+        player2Cards.push(removedCardP2);
+    } 
+
+    console.log(player2Cards);
+
+}
+
+const reset = () => {
+    player1CardNum = 0;
+    player2CardNum = 0;
+    player1Cards = [];
+    player2Cards = [];
+    
+    }
+    
+    
+
+
+
+
+const winningPlayer = () => { //POINTER - help with the loop to loop throughthe score function and declare a winner
+
+    while (player1Cards.length > 0 || player2Cards.length > 0) {
+        score();
+        if (player1Cards.length === 0) {
+            return ("Player 1 has won!")
+        }
+        if (player2Cards.length === 0) {
+        return ("Player 2 has won!")
+        }
+        reset(); 
+}
+}
+
+
+showCard();
+score();
+score();
+score();
+// winningPlayer();
+
+console.log("Player 1's cards are: " + JSON.stringify(player1Cards));
+console.log("Player 2's cards are " + JSON.stringify(player2Cards));
+// winningPlayer();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const playGame = () => {
+//     distributeCards();
+//     score();
+//     winningPlayer();
+// }
+
+// playGame();
+
+// console.log(player1CardNum);
+// console.log(player2CardNum);
+// have object in a string so can't print the usual way, have to add commar to seperate
+// console.log(`Player1's cards are: ', ${player1Cards[0]}`);
+// console.log(`Player2's cards are: ', ${player2Cards[0]}`);
+// console.log(cardDeck);
+
+//function if player1 wins
+// const score = () => {
+//     if (player1Cards[0].speed > players2Cards[0].speed) {
+//         player1CardNum++;
+//         player2CardNum--;
+//         let removedCardP1  = player1Cards.splice(0,1); 
+//         player1Cards.push(removedCardP1);
+        
+        
+//         //card and position of that card
+//     // } else if (player1Cards[0].speed === player2Cards[0].speed){ // creating a holdingpile
+//     //     const holdingPile = player1Cards.shift();
+
+//     } else {
+//         player1CardNum--;
+//         player2CardNum++;
+//         let removedCardP2 = player2Cards.splice(0,1);
+//         player2Cards.push(removedCardP2);
+//     } 
+// }
