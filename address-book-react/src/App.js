3b479525e2ca845f428204efb7ed2ce42d866f30
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-11 17:18:03 +0000

    address book react challenge 1st attempt

--- address-book-react/src/App.js
+++ address-book-react/src/App.js
@@ -3,35 +3,24 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    addresses: []
+    number: []
   }
 
-addAddress = () => {
+addAddress = (e) => {
+  this.setState({
+    number: e.target.value
+  });
 
 }
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
+render() {
   return (
    <div>
    <h1>Addresses</h1>
-   <input type="number"/>
-   <p></p>
-   
-
+   <input type="number" value={this.state.value}/>
+   <button>Submit</button>
+   <p>{this.state.number}</p>
    </div>
-  );
+  )}
 }
 
 export default App;
