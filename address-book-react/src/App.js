cf193b89ffb52be8b4570083c764aa249565226b
Emma Sheridan <emmasheridan@Emmas-MacBook-Air.local>
2020-02-11 22:38:44 +0000

    submitAddress & addAddress functions added

--- address-book-react/src/App.js
+++ address-book-react/src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    number: []
+    addressNumber: "",
+    addressBook: []
   }
 
 addAddress = (e) => {
   this.setState({
-    number: e.target.value
+    addressNumber: e.target.value // e.target can only be used on inputs
   });
-
 }
+
+submitAddress = () => {
+  let newAddressBook = [...this.state.addressBook]; //cloning the original address book as cant update the state directly in react
+  newAddressBook.push(this.state.addressNumber); //push new address number entries into the new address book
+  this.setState({
+    addressBook : newAddressBook //updating the state with the new address book values
+  })
+}
+
 render() {
   return (
    <div>
-   <h1>Addresses</h1>
-   <input type="number" value={this.state.value}/>
-   <button>Submit</button>
-   <p>{this.state.number}</p>
+    <h1>Addresses</h1>
+    <input type="number" value={this.state.addressNumber} onChange={this.addAddress} />
+    <button onClick={this.submitAddress}>Submit</button>
+    <p>{this.state.addressNumber}</p>
    </div>
   )}
 }
